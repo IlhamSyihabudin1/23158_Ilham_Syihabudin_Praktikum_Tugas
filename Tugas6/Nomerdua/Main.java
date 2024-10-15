7e58a88b47bebb4d7fb691e39fcfe29b6c3a20c3
IlhamSyihabudin1 <2310631170158@student.unsika.ac.id>
2024-10-15 17:13:49 +0700

    tugas 6

---
+++ Tugas6/Nomerdua/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Tugas6.Nomerdua;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan n yang anda inginkan : ");
+        int n = input.nextInt();
+        myFunction num = new myFunction(n);
+        
+        num.getOutput();
+    }
+  }
+    
---
+++ Tugas6/Nomersatu/Lagu.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas6.Nomersatu;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Lagu {
+    private int jumlahAnakAyam;
+    
+    public Lagu(int jumlah){
+        this.jumlahAnakAyam = jumlah;
+    }
+    
+    public void getOutput(){
+        System.out.println("Lagu Anak Ayam");
+
+        for (int i = jumlahAnakAyam; i > 0; i--) {
+            System.out.println("Mati satu tinggal "+ i);
+        }
+
+        System.out.println("Mati satu tinggal induknya");
+        System.out.println("Mati semuaaaa~~~");
+    }    
+    
+}
